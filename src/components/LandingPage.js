1e9acfd1502374d464baf38ad725ef3b20cc865a
Cody Perry <perrysdevelopment@gmail.com>
2022-04-06 05:54:05 -0400

    Add google fonts and update landing page styles

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,8 +4,7 @@ const LandingPage = () => {
     return (
         <main className="landing-page">
             <h1 className="title">Quizzical</h1>
-            <p className="description">Ready to play?</p>
-            <button className="start-btn">Start quiz</button>
+            <button className="start-btn shadow">Start quiz</button>
         </main>
     )
 }
