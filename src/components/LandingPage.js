781402cd20471ee5145e0f79e8cc84ffc4135d3a
Cody Perry <perrysdevelopment@gmail.com>
2022-04-06 05:20:32 -0400

    Add initial app structure

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return (
+        <main className="landing-page">
+            <h1 className="title">Quizzical</h1>
+            <p className="description">Ready to play?</p>
+            <button className="start-btn">Start quiz</button>
+        </main>
+    )
+}
+
+export default LandingPage
